21720223f2ef79b92770e134594a1b22e890b737
Adam Gibson <adamhenrygibson@gmail.com>
2023-03-18 13:17:12 +0000

    Updated fixtures with Location to pass tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -38,6 +38,7 @@ export const musicCategory = {
 export const location = {
   placemarkName: "NUIM",
   description: "my first college",
+  location: "12.45 -6.34",
 };
 
 export const testCategories = [
@@ -60,14 +61,17 @@ export const testPlacemarks = [
   {
     placemarkName: "Moes",
     description: "Tavern",
+    location: "35.75 -12.34",
   },
   {
     placemarkName: "Bushy Park",
     description: "park",
+    location: "21.45 -7.34",
   },
   {
     placemarkName: "The boat",
     description: "rivers",
+    location: "45.12 -67.30",
   },
 ];
 
